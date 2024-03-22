c92811d1e3c5968554d61e3ca0badd8af6b44728
Tanishq112005 <148210922+Tanishq112005@users.noreply.github.com>
2024-03-22 15:48:02 +0530

    solution.cpp
    Solution Of Easy Level Problem

--- Easy/Day1/sulution.cpp
+++ Easy/Day1/sulution.cpp
@@ -1,1 +1,28 @@
-// Write your solution here
+// Write your solution here\
+#include<bits/stdc++.h>
+int function3(int a ){
+    int count = 0;
+    for(int x = 1 ;x<=sqrt(a) ; x++){
+        if(a%x==0){
+       count++;
+        }
+    }
+    return count ;
+}
+int function2(int p){
+     int h = 1;
+     while(1){
+        if(function3(h)>=p){
+             return h;
+             break;
+        }
+        h++;
+     }
+}
+using namespace std;
+int main(){
+    int p ;
+    cin>>p;
+    cout<<function2(p);
+    return 0;
+}
