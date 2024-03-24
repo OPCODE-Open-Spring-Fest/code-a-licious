3991683467631969a196d7517378f419726051da
Isha1233 <120541874+Isha1233@users.noreply.github.com>
2024-03-24 16:18:56 +0530

    chore; added soln

--- Easy/Day 2/solution.cpp
+++ Easy/Day 2/solution.cpp
@@ -1,1 +1,35 @@
 // Write your code here
+// Write your code here
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+    int n = INT_MAX;
+    int flag = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int count = 0;
+        for (int j = 1; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                count++;
+            }
+            if (count == k)
+            {
+                cout << i << endl;
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
