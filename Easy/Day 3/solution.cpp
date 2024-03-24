28b80b47fdcdac5d12161ba49119fd080dbbfc84
Isha1233 <120541874+Isha1233@users.noreply.github.com>
2024-03-24 20:59:09 +0530

    Update EasyDay3 solution.cpp

--- Easy/Day 3/solution.cpp
+++ Easy/Day 3/solution.cpp
@@ -1,1 +1,22 @@
 // Write your code here
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x1,y1,r1,x2,y2,r2;
+  cin>>x1>>y1>>r1>>x2>>y2>>r2;
+ long long check=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+
+if(check<r1+r2){
+    cout<<"2"<<endl;
+}
+else if(check==r1+r2){
+    cout<<"-1"<<endl;
+}
+else{
+    cout<<"0"<<endl;
+}
+ 
+
+    return 0;
+}
