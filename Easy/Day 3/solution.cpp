3f96b8f8c73d8820e44f1ee97aa9c8532e5b2c04
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-04-02 00:53:38 +0530

    feat: solved day 3 easy

--- Easy/Day 3/solution.cpp
+++ Easy/Day 3/solution.cpp
@@ -1,24 +1,23 @@
 // Write your code here
+// Code by Jagpreet153
+// Approach : Math
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    float x1,y1,r1,x2,y2,r2;
-  cin>>fixed >> setprecision(3)>>x1>>y1>>r1>>x2>>y2>>r2;
-  float check=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-if(check>r1+r2 || check<fabs(r1-r2)){
-    cout<<"0"<<endl;
-}
-else if(check<r1+r2){
-    cout<<"2"<<endl;
-}
-else if(check==r1+r2 || check==fabs(r1-r2)){
-    cout<<"1"<<endl;
-}
-else{
-    cout<<"-1"<<endl;
-}
- 
-
+    double x1, y1, r1, x2, y2, r2;
+    cin>>x1>>y1>>r1>>x2>>y2>>r2;
+    double dist = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    if (dist > r1 + r2) 
+        cout << 0 << endl;  // Circles are separate
+    else if (dist == r1 + r2) 
+        cout << 1 << endl;  // Circles touch each other externally
+     else if (dist + min(r1, r2) < max(r1, r2)) 
+        cout << 0 << endl;  // One circle is inside the other and not touching
+    else if (dist==0) 
+        cout << -1 << endl;  // Circles coincide
+    else if (dist + min(r1, r2) == max(r1, r2)) 
+        cout << 1 << endl;  // Circles touch each other internally
+     else 
+        cout << 2 << endl;  // Circles intersect at two points
     return 0;
 }
