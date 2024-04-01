7530ad32440e3541b281e1f46db64d95da114d47
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-04-01 21:04:26 +0530

    feat: solved day 1 medium

--- Medium/Day1/solution.cpp
+++ Medium/Day1/solution.cpp
@@ -1,8 +1,21 @@
-#include<iostrem>
+// Code by Jagpreet153
+// Approach maths and general observation
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-  // your code here
-
-  return 0;
+int main() {
+    int S,K,A,B;
+    cin>>S>>K>>A>>B;
+    for(int D=1;D<=A;D++) {
+        int C=A-K*D;
+        if (C<=0) {
+            continue;
+        }
+        int N =(B-S*C)/D;
+        if (S*C+N*D == B && N >= 0) {
+            cout<< N<<endl;
+            return 0;
+        }
+    }
+    cout<<-1<<endl;
+    return 0;
 }
