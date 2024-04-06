2f002814fbab0c243d784eb32bf3413dae31e7ae
ashutosh <ashutosh.2201062cs@iiitbh.ac.in>
2024-04-06 23:46:58 +0530

    chore: solved medium day 10 task

--- Medium/Day 10/solution.cpp
+++ Medium/Day 10/solution.cpp
@@ -1,53 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long int
 
-
-
-float sq(float n)
-{
-    float x = n;
-    float y = 1;
- 
-    float e = 0.000001; 
-    while (x - y > e)
-    {
-        x = (x + y) / 2;
-        y = n/x;
-    }
-    return x;
-}
- 
-int fun(int m)
-{
-    int n = 1 + 8*m;
-    int mx = (-1 + sq(n)) / 2;
-    return mx;
-}
-
-void solve()
-{ 
-    int k ;
-    cin>>k;
-    cout << fun(k) << endl;
-    return ;
-}
-
-
-    
-        
-
-
-signed main() 
+int main() 
 {
 	int t;
-	cin>>t;
-    // cout<<t<<endl;
+	cin >> t;
 	while(t--)
-    {
-        solve();   
-    }
+	{
+	    long long int n;
+	    cin >> n;
+	    
+	   long long  ans= (sqrt(1 + 8*n) - 1)/2;
+	    cout << ans << endl;
+	}
 	return 0;
 }
-
-  
