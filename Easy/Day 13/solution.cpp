327d4c96fbd57d5689ead474d13c3f0fe753cc6f
Tanishq112005 <148210922+Tanishq112005@users.noreply.github.com>
2024-04-07 09:55:04 +0530

    feat: solved day 13 easy

--- Easy/Day 13/solution.cpp
+++ Easy/Day 13/solution.cpp
@@ -1,1 +1,22 @@
 // your code here
+#include <bits/stdc++.h>
+using namespace std;
+
+int isPalindrome(int N) {
+    string str = to_string(N);
+    for (int i = 0; i < str.length() / 2; i++) {
+        if (str[i]!= str[str.length() - i - 1]) {
+            return -1;
+        }
+    }
+
+    return 1;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << isPalindrome(N) << endl;
+
+    return 0;
+}
