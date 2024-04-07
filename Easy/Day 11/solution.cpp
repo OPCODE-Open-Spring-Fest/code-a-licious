25156902839564df235c1750a9e933e8da5d06e1
Tanishq112005 <148210922+Tanishq112005@users.noreply.github.com>
2024-04-07 09:19:41 +0530

    feat: solved day 11 easy

--- Easy/Day 11/solution.cpp
+++ Easy/Day 11/solution.cpp
@@ -1,1 +1,39 @@
 // your code here
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int minOperations(vector<int>& arr) {
+    unordered_map<int, int> frequency;
+    int maxFreq = 0;
+    
+
+    for (int num : arr) {
+        frequency[num]++;
+        maxFreq = max(maxFreq, frequency[num]);
+    }
+    
+   
+    return arr.size() - maxFreq;
+}
+
+int main() {
+    int t;
+    cin >> t; 
+    
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        
+    
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        
+      
+        cout << minOperations(arr) << endl;
+    }
+    
+    return 0;
+}
