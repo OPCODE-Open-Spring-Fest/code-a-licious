f183ca7dd7d057205ca3dbe7ac07f6ab53e9da88
Tanishq112005 <148210922+Tanishq112005@users.noreply.github.com>
2024-04-07 22:06:08 +0530

    feat: solved day 12 easy

--- Easy/Day 12/solution.cpp
+++ Easy/Day 12/solution.cpp
@@ -1,1 +1,31 @@
 // your code here
+#include<bits/stdc++.h>
+using namespace std;
+string Good(string s) {
+        stack<char> st;
+        for (auto& ch : s) {
+            if (!st.empty() && ch != st.top() &&
+                (toupper(ch) == st.top() || ch == toupper(st.top())))
+                st.pop();
+            else
+                st.push(ch);
+        }
+        string output = "";
+        while (!st.empty()) {
+            output = st.top() + output;
+            st.pop();
+        }
+        return output;
+    }
+int main(){
+string s ;
+cin>>s;
+string k  = Good(s);
+if(k.empty()){
+    cout<<-1<<endl;
+}
+else{
+    cout<<k<<endl;
+}
+   return 0;
+}
