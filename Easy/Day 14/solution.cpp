72fab605cddc0aeec62ff9490c02847f9f814d8f
ayushcodes0 <auyshsingh94@gmail.com>
2024-04-09 19:47:28 +0530

    feat: solution of day 14 easy

--- Easy/Day 14/solution.cpp
+++ Easy/Day 14/solution.cpp
@@ -1,1 +1,15 @@
 // Write your code here
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    if(s1.find(s2) == string::npos) {
+        cout<<-1<<endl;
+    }   
+    else{
+        cout<<s1.find(s2)<<endl;
+    }
+    return 0; 
+}
