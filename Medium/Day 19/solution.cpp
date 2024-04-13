01aac4acc5674105729887a65140b803a68c54c1
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-04-14 00:28:07 +0530

    feat: solved day 19 medium

--- Medium/Day 19/solution.cpp
+++ Medium/Day 19/solution.cpp
@@ -1,1 +1,23 @@
 // Write your code here.
+// Code by Jagpreet153
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main() {
+int n, x ; 
+cin >> n >> x;
+  int a[n + 1];
+  for(int i = 1; i <= n; i++) cin >> a[i];
+  int ans = 0;
+  for(int i = 2; i <= n; i++) {
+    if(a[i - 1] + a[i] > x) {
+        int temp = a[i - 1] + a[i];
+        temp = temp - x;
+        ans += temp;
+        if(temp > a[i]) a[i] = 0;
+        else a[i] -= temp;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
