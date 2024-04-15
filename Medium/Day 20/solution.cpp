3db9d45cf4fb7c25739590f53c7a67d4d42384b9
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-04-15 12:34:16 +0530

    feat: solved day 20 medium

--- Medium/Day 20/solution.cpp
+++ Medium/Day 20/solution.cpp
@@ -1,1 +1,50 @@
 // Write your code here.
+//Code by Jagpreet153
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+
+int main() 
+{
+    int h, w; 
+    cin >> h >> w;
+    vector<vector<int> > m(h, vector<int>(w)), dp(h, vector<int>(w, 0));
+    for(int i = 0; i < h; i++) 
+    {
+        string s; 
+        cin >> s;
+        for(int j = 0; j < w; j++) {
+            if(s[j] == '#') m[i][j] = 1;
+            else m[i][j] = 0;
+        }
+    }
+    if(m[0][0] == 1) dp[0][0] = 1;
+    else dp[0][0] = 0;
+
+    for(int i = 0; i < h; i++) for(int j = 0; j < w; j++) 
+    {
+        if(i == 0 && j == 0) 
+        continue;
+        if(i == 0) 
+        {
+            if(m[i][j-1]==0 && m[i][j]) 
+            dp[i][j] = dp[i][j-1] + 1;
+            else
+            dp[i][j] = dp[i][j-1];
+        } 
+        else if(j == 0) 
+        {
+            if(m[i-1][j]==0 && m[i][j])
+            dp[i][j] = dp[i-1][j] + 1;
+            else 
+            dp[i][j] = dp[i-1][j];
+        } 
+        else 
+        {
+            dp[i][j] = min(dp[i-1][j]+((m[i-1][j]==0 && m[i][j])?1:0), dp[i][j-1]+((m[i][j-1]==0 && m[i][j])?1:0));
+        }
+    }
+
+    cout << dp[h-1][w-1] << endl;
+    return 0;
+}   
