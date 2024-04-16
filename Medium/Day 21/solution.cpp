bf6af999eaa7eab33fda2d86b255dd84f9b906ef
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-04-16 12:48:18 +0530

    feat: solved day 21 medium

--- Medium/Day 21/solution.cpp
+++ Medium/Day 21/solution.cpp
@@ -1,1 +1,23 @@
 // Write your code here.
+// Code By Jagpreet153
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n,a[200010],b[200010],c[200010];
+set<int> ans;
+int main(){
+	cin>>n;
+	for(int i=1;i<=n;i++) cin>>a[i];
+	for(int i=1;i<=n;i++) cin>>b[i];
+	sort(b+1,b+n+1);
+	for(int i=1;i<=n;i++){
+		int x=a[i]^b[1],flg=1;
+		for(int j=1;j<=n;j++) c[j]=x^a[j];
+		sort(c+1,c+n+1);
+		for(int j=1;j<=n;j++) if(c[j]!=b[j]) flg=0;
+		if(flg) ans.insert(x);
+	}
+	cout<<ans.size()<<endl;
+	for(int x:ans) cout<<x<<endl;
+	return 0;
+}
